dbef9c34e42413608452eaab28185eabfa974633
marioaruiz97 <marioarb.97@gmail.com>
2022-06-15 14:26:21 -0500

    se inicia footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,12 +6,13 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
 import { SeEncuentraEnComponent } from './components/se-encuentra-en/se-encuentra-en.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
-  declarations: [SidenavComponent, ToolbarComponent, ConfirmDialogComponent, SeEncuentraEnComponent],
+  declarations: [SidenavComponent, ToolbarComponent, ConfirmDialogComponent, SeEncuentraEnComponent, FooterComponent],
   imports: [CommonModule, RouterModule, SharedModule],
-  exports: [SidenavComponent, ToolbarComponent, SeEncuentraEnComponent],
+  exports: [SidenavComponent, ToolbarComponent, SeEncuentraEnComponent, FooterComponent],
   providers: [],
 })
 export class CoreModule { }
